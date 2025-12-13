07257d3c4583e1bd060cd0385a569c27fc5bd5a8
kotr228 <trockijkostantin@gmail.com>
2025-12-13 06:17:34 +0200

    public-demo

--- Geocadastr_0_1/DocControlSolution/DocControlUI/obj/Debug/net8.0-windows/DocControlUI.AssemblyInfo.cs
+++ Geocadastr_0_1/DocControlSolution/DocControlUI/obj/Debug/net8.0-windows/DocControlUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DocControlUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27db9bb2729a6cf0ba3cea85e825f6e409d864e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45c780457c4d1adda50699ca4e670ae581dc2844")]
 [assembly: System.Reflection.AssemblyProductAttribute("DocControlUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DocControlUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
