48b72c5cccd0698aa7d4ec5496f1b0db2ba7e36c
kotr228 <trockijkostantin@gmail.com>
2026-09-05 12:00:20 +0300

    start

--- JolieCat/JolieCat.UI/ViewModels/MainViewModel.cs
+++ JolieCat/JolieCat.UI/ViewModels/MainViewModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace JolieCat.UI.ViewModels
 {
-    internal class MainViewModel
+    public partial class MainViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private bool isLeftPanelVisible = true;
+
+        [ObservableProperty]
+        private bool isRightPanelVisible = true;
+
+        [ObservableProperty]
+        private bool isBottomPanelVisible = true;
+
+        [RelayCommand]
+        public void ToggleLeftPanel()
+        {
+            IsLeftPanelVisible = !IsLeftPanelVisible;
+        }
+
+        [RelayCommand]
+        public void ToggleRightPanel()
+        {
+            IsRightPanelVisible = !IsRightPanelVisible;
+        }
+
+        [RelayCommand]
+        public void ToggleBottomPanel()
+        {
+            IsBottomPanelVisible = !IsBottomPanelVisible;
+        }
     }
 }
